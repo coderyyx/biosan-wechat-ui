19689dcd08720cebaea319dde093766cbeb6f74d
yyx <yyxcoder@163.com>
2018-03-23 17:18:18 +0800

    add dialog

---
+++ components/dialog/index.js
@@ -0,0 +1,17 @@
+
+/*
+show 弹出框
+$bs_dialog_cb:(type,dataset)
+回调函数 事件委托 参数：type ，dataset
+*/
+const dialogController = {
+    $bs_dialog_init : function(){
+
+    },
+    $bs_dialog_listener:function(){
+
+    },
+    $bs_dialog_show:function(options = {}){
+
+    }
+}
