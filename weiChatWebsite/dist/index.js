8e69f2a7d6735011c9e30249b2c743e0b694421f
yyx <yyxcoder@163.com>
2018-03-30 17:39:06 +0800

    add cascader

--- weiChatWebsite/dist/index.js
+++ weiChatWebsite/dist/index.js
@@ -3,11 +3,13 @@ import { bsToastController} from "./toast/index.js";
 import { dialogController} from "./dialog/index.js";
 import { loadingController} from "./loading/index.js";
 import { bsActionSheetController} from "./actionSheet/index.js";
+import { cascaderController} from "./cascader/index.js";
 
 export {
     bsTipsControll,
     bsToastController,
     dialogController,
     loadingController,
-    bsActionSheetController
+    bsActionSheetController,
+    cascaderController
 }
