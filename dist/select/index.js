6abbd43b7befcba4c1a7d5a3efdda3d62c54822c
yinluer <hduyejie@163.com>
2018-04-03 18:52:29 +0800

    yj

--- dist/select/index.js
+++ dist/select/index.js
@@ -1,7 +1,25 @@
+//回填选中项
+const backFill = (valueList,actions)=>{
+  if(valueList==[]){
+    return actions;
+  }else{
+    for(let i=0;i<valueList.length;i++){
+      for(let j=0;j<actions.length;j++){
+        if(actions[j].value==valueList[i]){
+          actions[j].flag=true;
+          break;
+        }
+      }
+    }
+    return actions;
+  }
+}
+
 const bsSelectController = {
   $bs_select_init:function(options = {}){
     let {
       valueList = [],
+      type = 'radio',
       actions = [
             {
                 descp:'option1',
@@ -18,38 +36,53 @@ const bsSelectController = {
       ]
       
     } = options;
-
+    console.log(11);
+    let yjactions = backFill(valueList,actions);
     // init
     this.setData({
       bs_select:{
         valueList,
-        actions
+        actions:backFill(valueList,actions),
+        type
       }
     });
   },
   $bs_select_selected:function({ currentTarget = {} }){
     const dataset = currentTarget.dataset || {};
-    console.log(dataset);
     const { index, item } = dataset;
-
-    let valueList = this.data.bs_select.valueList;
-    let actions = this.data.bs_select.actions;
+    let {valueList,actions,type} = this.data.bs_select;
     let valueFlag = actions[index].flag;
-    
-    if(!valueFlag){
-        valueList.splice(index,0,item.value); 
-    }else{
-        valueList.splice(index,1);
-    }
-    actions[index].flag = valueFlag?false:true;
-    this.setData({
-        bs_select:{
-            actions:actions,
-            valueList:valueList
+
+    let updateObj = {};
+
+    if(type=='checkbox'){
+      if(!valueFlag){
+          valueList.push(item.value); 
+      }else{
+          valueList.splice(valueList.indexOf(item.value),1);
+      }
+      actions[index].flag = valueFlag?false:true;
+      updateObj = Object.assign({},this.data.bs_select,{actions,valueList});
+    }else if(type=='radio'){
+      valueList = [];
+      for(let i=0;i<actions.length;i++){
+        if(index==i){
+          actions[i].flag = valueFlag?false:true;
+          if(!valueFlag)
+            valueList.push(item.value);
+        }else{
+          actions[i].flag = false;
         }
+      }
+      updateObj = Object.assign({},this.data.bs_select,{actions,valueList});
+    }else{
+      console.warn('不支持此类型');
+    }
+    this.setData({
+        bs_select:updateObj
     })
     
-    
+    console.log(this.data.bs_select.valueList);
     if (this.btnSelect) {
       this.btnSelect(this.data.bs_select.valueList);
     } else {
